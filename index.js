7c8e14f923178500eabe00d7a22f466b7c806807
starptech <deusdustin@gmail.com>
2020-08-16 17:15:54 +0200

    test

--- index.js
+++ index.js
@@ -200,6 +200,7 @@ function fastifyMultipart (fastify, options = {}, done) {
     }
 
     function onEnd (error) {
+      console.log('####################', error, lastError);
       cleanup()
       bb.removeListener('finish', onEnd)
       bb.removeListener('error', onEnd)
