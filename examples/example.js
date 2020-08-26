2cfac6ed260d46452e0ef204dc933b2ad2de5daa
Dustin Deus <deusdustin@gmail.com>
2020-08-26 14:22:33 +0200

    fix docs examples (#145)

--- examples/example.js
+++ examples/example.js
@@ -29,7 +29,7 @@ fastify.post('/upload/stream/files', async function (req, reply) {
 })
 
 fastify.post('/upload/raw/any', async function (req, reply) {
-  const parts = await req.multipart()
+  const parts = await req.multipartIterator()
   for await (const part of parts) {
     if (part.file) {
       await pump(part.file, fs.createWriteStream(part.filename))
