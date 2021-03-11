63bc66e94f9c9359487c31026f278e5d697339cf
Vladimir Adamic <vladimir.adamic@gmail.com>
2021-03-11 12:57:35 +0100

    Fix test failures
    - move throwFileSizeLimit declaration to its original place

--- index.js
+++ index.js
@@ -149,6 +149,8 @@ function fastifyMultipart (fastify, options, done) {
     })
   }
 
+  let throwFileSizeLimit = true
+
   const PartsLimitError = createError('FST_PARTS_LIMIT', 'reach parts limit', 413)
   const FilesLimitError = createError('FST_FILES_LIMIT', 'reach files limit', 413)
   const FieldsLimitError = createError('FST_FIELDS_LIMIT', 'reach fields limit', 413)
@@ -397,7 +399,6 @@ function fastifyMultipart (fastify, options, done) {
         return
       }
 
-      let throwFileSizeLimit = true
       if (typeof opts.throwFileSizeLimit === 'boolean') {
         throwFileSizeLimit = opts.throwFileSizeLimit
       }
