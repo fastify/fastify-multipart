dd15014a060463188200ebaa7561733c0806c3ab
Igor Savin <iselwin@gmail.com>
2021-12-08 15:01:37 +0200

    Fix TS types (#305)

--- index.d.ts
+++ index.d.ts
@@ -54,17 +54,17 @@ declare module "fastify" {
         isMultipart: () => boolean;
 
         // promise api
-        parts: (options?: BusboyConfig) =>  AsyncIterableIterator<Multipart>
+        parts: (options?: Omit<BusboyConfig, 'headers'>) =>  AsyncIterableIterator<Multipart>
 
         // legacy
-        multipart: (handler: MultipartHandler, next: (err: Error) => void, options?: BusboyConfig) => Busboy;
+        multipart: (handler: MultipartHandler, next: (err: Error) => void, options?: Omit<BusboyConfig, 'headers'>) => Busboy;
 
         // Stream mode
-        file: (options?: BusboyConfig) => Promise<Multipart>
-        files: (options?: BusboyConfig) => AsyncIterableIterator<Multipart>
+        file: (options?: Omit<BusboyConfig, 'headers'>) => Promise<Multipart>
+        files: (options?: Omit<BusboyConfig, 'headers'>) => AsyncIterableIterator<Multipart>
 
         // Disk mode
-        saveRequestFiles: (options?: BusboyConfig & { tmpdir?: string }) => Promise<Array<Multipart>>
+        saveRequestFiles: (options?: Omit<BusboyConfig, 'headers'> & { tmpdir?: string }) => Promise<Array<Multipart>>
         cleanRequestFiles: () => Promise<void>
         tmpUploads: Array<Multipart>
     }
@@ -90,6 +90,16 @@ export interface FastifyMultipartBaseOptions {
      */
     throwFileSizeLimit?: boolean
 
+    /**
+     * Detect if a Part is a file.
+     *
+     * By default a file is detected if contentType
+     * is application/octet-stream or fileName is not
+     * undefined.
+     *
+     * Modify this to handle e.g. Blobs.
+     */
+    isPartAFile?: (fieldName: string | undefined, contentType: string | undefined, fileName: string | undefined) => boolean;
 
     limits?: {
         /**
