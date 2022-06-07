7ba1777dee14ad4d88d1d36069a547a0da24e822
Matthew Little <zone117x@gmail.com>
2022-06-07 21:37:21 +0200

    test: include application/json content-type file upload test

--- test/multipart-attach-body.test.js
+++ test/multipart-attach-body.test.js
@@ -60,6 +60,52 @@ test('should be able to attach all parsed fields and files and make it accessibl
   t.pass('res ended successfully')
 })
 
+test('should be able to attach all parsed field values and json content files and make it accessible through "req.body"', async function (t) {
+  t.plan(6)
+
+  const fastify = Fastify()
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.register(multipart, { attachFieldsToBody: 'keyValues' })
+
+  const original = { testContent: 'test upload content' }
+
+  fastify.post('/', async function (req, reply) {
+    t.ok(req.isMultipart())
+
+    t.same(Object.keys(req.body), ['upload', 'hello'])
+
+    t.same(req.body.upload, original)
+    t.equal(req.body.hello, 'world')
+
+    reply.code(200).send()
+  })
+
+  await fastify.listen({ port: 0 })
+
+  // request
+  const form = new FormData()
+  const opts = {
+    protocol: 'http:',
+    hostname: 'localhost',
+    port: fastify.server.address().port,
+    path: '/',
+    headers: form.getHeaders(),
+    method: 'POST'
+  }
+
+  const req = http.request(opts)
+  form.append('upload', Readable.from(Buffer.from(JSON.stringify(original))), { contentType: 'application/json' })
+  form.append('hello', 'world')
+  form.pipe(req)
+
+  const [res] = await once(req, 'response')
+  t.equal(res.statusCode, 200)
+  res.resume()
+  await once(res, 'end')
+  t.pass('res ended successfully')
+})
+
 test('should be able to attach all parsed field values and files and make it accessible through "req.body"', async function (t) {
   t.plan(6)
 
