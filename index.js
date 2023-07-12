ff4eff0fbe752e66b24dc77a952b0dc4d1de4c87
alifuma92 <44839296+alifuma92@users.noreply.github.com>
2023-07-12 14:57:07 +0200

    Update index.js
    when options.attachFieldsToBody is set to "keyValues" do not stringify buffer

--- index.js
+++ index.js
@@ -172,12 +172,12 @@ function fastifyMultipart (fastify, options, done) {
           } else if (Array.isArray(field)) {
             body[key] = field.map(item => {
               if (item._buf !== undefined) {
-                return item._buf.toString()
+                return item._buf
               }
               return item.value
             })
           } else if (field._buf !== undefined) {
-            body[key] = field._buf.toString()
+            body[key] = field._buf
           }
         }
         req.body = body
