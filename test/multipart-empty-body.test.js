e7102b3b84ebc3e48d846ad79aa2ce55d19d2639
Philipp Katz <philipp@philippkatz.de>
2023-07-17 11:37:39 +0200

    fix: Empty body handling when attachFieldsToBody is keyValues (#450)

--- test/multipart-empty-body.test.js
+++ test/multipart-empty-body.test.js
@@ -48,3 +48,45 @@ test('should not break with a empty request body when attachFieldsToBody is true
   await once(res, 'end')
   t.pass('res ended successfully')
 })
+
+test('should not break with a empty request body when attachFieldsToBody is keyValues', async function (t) {
+  t.plan(5)
+
+  const fastify = Fastify()
+  t.teardown(fastify.close.bind(fastify))
+
+  fastify.register(multipart, { attachFieldsToBody: 'keyValues' })
+
+  fastify.post('/', async function (req, reply) {
+    t.ok(req.isMultipart())
+
+    const files = await req.saveRequestFiles()
+
+    t.ok(Array.isArray(files))
+    t.equal(files.length, 0)
+
+    reply.code(200).send()
+  })
+
+  await fastify.listen({ port: 0 })
+
+  // request
+  const form = new FormData()
+  const opts = {
+    protocol: 'http:',
+    hostname: 'localhost',
+    port: fastify.server.address().port,
+    path: '/',
+    headers: form.getHeaders(),
+    method: 'POST'
+  }
+
+  const req = http.request(opts)
+  form.pipe(req)
+
+  const [res] = await once(req, 'response')
+  t.equal(res.statusCode, 200)
+  res.resume()
+  await once(res, 'end')
+  t.pass('res ended successfully')
+})
