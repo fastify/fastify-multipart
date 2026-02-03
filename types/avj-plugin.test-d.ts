3caa6a6c752eaf1d4ea3f8a2c5e4d1154f4d47aa
나형진 <skgudwls@konkuk.ac.kr>
2026-02-03 15:52:51 +0900

    test: add explicit type test for ajvFilePlugin return type
    Added expectType<Ajv> assertion to verify ajvFilePlugin correctly returns Ajv instance.

--- types/avj-plugin.test-d.ts
+++ types/avj-plugin.test-d.ts
@@ -1,6 +1,9 @@
 import fastify from 'fastify'
+import { expectType } from 'tsd'
+import type Ajv from 'ajv'
 import { fastifyMultipart, ajvFilePlugin } from '..'
 
+// Test: ajvFilePlugin should be compatible with Fastify's ajv.plugins option
 const app = fastify({
   ajv: {
     plugins: [
@@ -11,3 +14,7 @@ const app = fastify({
 })
 
 app.register(fastifyMultipart)
+
+// Test: ajvFilePlugin should accept Ajv and return Ajv
+declare const ajv: Ajv
+expectType<Ajv>(ajvFilePlugin(ajv))
